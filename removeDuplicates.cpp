936388158dae39bb19ac15de485323e6c7d6849c
Kacem Khalife <91025619+kskhalif@users.noreply.github.com>
2022-01-15 18:17:33 -0500

    01/15/2022

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,34 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-    
-int removeDuplicates(vector<int>& nums) 
-{
-    if (nums.empty()) return 0;
-    if (nums.size() == 1) return 1;
+#include <vector>
 
-    for (int i = nums.size() - 1; i > 0; i--)
-    {
-        if (nums[i] == nums[i - 1])
-        {
-            for (int j = i; j < nums.size() - 1; j++)
-                swap(nums[j], nums[j+1]);
-            nums.pop_back();       
-        }
-    }
-    return nums.size();
-}
-
-int removeDuplicates(vector<int>& nums) 
+template<typename T>
+void removeDuplicates(std::vector<T>& list) 
 {
-    if (nums.size() == 0)
+    if (list.size() == 0)
         return 0;
+    if (list.size() == 1)
+        return 1;
         
-    int k = 0;
+    int j = 0;
         
-    for (int i = 0; i < nums.size() - 1; i++)
-        if (nums[i] != nums[i+1])
-            nums[k++] = nums[i];
+    for (int i = 0; i < list.size() - 1; i++)
+        if (list[i] != list[i + 1])
+            list[j++] = list[i];
               
-    nums[k++] = nums[nums.size()-1];
-    return k;
+    list[j++] = list[list.size() - 1];
+
+    list.erase(list.begin() + j, list.begin() + list.size());
 }
